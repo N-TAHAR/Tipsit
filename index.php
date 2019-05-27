1da948ef9295ceea878a05ac68f640dde2b298fe
Najib Tahar <najibtaharberrabah@gmail.com>
2019-05-27 18:54:50 +0200

    retrieve tips from database to index.php & style.css added

--- index.php
+++ index.php
@@ -14,6 +14,23 @@
       echo '<p>Inscris toi <a href="register.php">ici</a> pour te faire connaître.</p>';
     }
 
+  
+  $req = $pdo -> prepare(
+    'SELECT *
+    FROM posts
+    '
+  );
+
+  $req -> execute();
+
+  while($donnees = $req -> fetch()){
+    echo '<div class="tip">';
+    echo '<p>' . strip_tags(($donnees['username'])) . '</p>';
+    echo '<p>' . strip_tags(($donnees['content'])) . '</p>';
+    echo '<time>' . strip_tags(($donnees['date'])) . '</time>';
+    echo '<p>' . '#' . strip_tags(($donnees['keyword'])) . '</p>';
+    echo '</div>'; 
+  }
   ?>
   
 </body>
