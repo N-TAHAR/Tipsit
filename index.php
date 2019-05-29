9672808a0f16344002d84342f15aee29e745237b
unknown <felix.robaglia@gmail.com>
2019-05-29 02:18:48 +0200

    increment clap counter on client side

--- index.php
+++ index.php
@@ -12,17 +12,17 @@
   foreach ($tips as $tip) {
   App\Entity\Tip::setTipProps($tip->username, $tip->content, $tip->keyword, $tip->date, $tip->id, $tip->claps)
   ?>
-  <article class="post">
+  <article class="post" id="<?php echo App\Entity\Tip::getId() ?>">
     <h2> <?php echo '#' . App\Entity\Tip::getKeyword() ?> </h2>
     <p class="article"> <?php echo App\Entity\Tip::getContent() ?> </p>
     <div class="bottom">
       <div class="bottomlikes">
-        <div class="like" onclick="ajax(<?php echo App\Entity\Tip::getId() ?>)"></div>
-        <p class="bulb"> <?php echo App\Entity\Tip::getClaps()?> bulb</p>
+        <div class="like"  onclick="ajax(<?php echo App\Entity\Tip::getId() ?>)"></div>
+        <p class="bulb"> <span class="bulbNumber"><?php echo App\Entity\Tip::getClaps() ?></span>&nbsp;bulb</p>
       </div>
       <p class="name"> <?php echo App\Entity\Tip::getUsername() ?> </p>
     </div>
-    <!-- <div> <(?php echo App\Entity\Tip::getDate() ?> </div> -->
+    <span class="date"><?php echo App\Entity\Tip::getDate() ?> </span>
   </article>
   <?php 
   }
